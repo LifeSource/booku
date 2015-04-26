6af5cad67c975bdd9f8bc354a6a010d9d7d685b2
Kenrick Wu <kenrick.wu@gmail.com>
2015-04-27 01:37:59 +0800

    Setup the database with seed data.

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,8 +1,19 @@
 var mongoose = require("mongoose");
+var Book = require("../models/book");
+var seedData = require("../models/seedData");
 
 module.exports = function(config) {
 
 	mongoose.connect(config.db);
 
 	var database = mongoose.connection;
+
+	Book.find({}).exec(function (err, books) {
+		if (books.length === 0) {
+			console.log("\tSeeding database ...");
+			database.collection("books").insert(seedData.books);		
+		} else {
+			console.log("\tDatabase is already seeded ...");
+		}
+	});
 };
---
+++ models/seedData.js
@@ -0,0 +1,30 @@
+(function(seedData) {
+
+    "use strict";
+
+    seedData.books = [
+		{
+            title: "NodeJS in Action",
+            author: "Mike Cantelon",
+            genre: "Computing",
+            price: 35.0
+        }, {
+            title: "C# in Depth, 3rd Edition",
+            author: "Jon Skeet",
+            genre: "Computing",
+            price: 45.0
+        }, {
+            title: "HTTP Succinctly",
+            author: "Scott Allen",
+            genre: "Computing",
+            price: 0.0
+        }, {
+            title: "Clean Code",
+            author: "Robert C. Martin",
+            genre: "Computing",
+            price: 55.0
+        }
+
+    ];
+
+}(module.exports));
